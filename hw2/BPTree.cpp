22c3fb705b8d771f24cc1c43ff547a65a83d09f2
tgrabham <tgrabham@sfu.ca>
2021-06-24 01:30:44 -0700

    please god let this work

--- hw2/BPTree.cpp
+++ hw2/BPTree.cpp
@@ -448,6 +448,15 @@ int main() {
   node.insert(40);
   node.insert(30);
   node.insert(20);
+  node.insert(1);
+  node.insert(2);
+  node.insert(3);
+  node.insert(4);
+  node.insert(6);
+  node.insert(7);
+  node.insert(8);
+  node.insert(9);
+  node.insert(10);
   node.display(node.getRoot());
   cout << endl << endl;
   node.deletes(40);
