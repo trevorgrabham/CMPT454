667d3d1fec4bc7a4ca2f41366311d16426da7162
tgrabham <tgrabham@sfu.ca>
2021-06-24 00:57:37 -0700

    please god let this work

--- hw2/BPTree.cpp
+++ hw2/BPTree.cpp
@@ -15,7 +15,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <math.h>
 using namespace std;
 int MAX = 3;
 
@@ -245,7 +244,7 @@ void BPTree::deleteInternal(Node* parent, int cursorI, int x){
   }
     // else, check the size of cursor
       // if size >= MAX/2 + 1 
-  if(cursor->size >= ceil(MAX/2)+1){
+  if(cursor->size >= MAX/2.0+1){
     cout << "\nJust deleting\tNode size is " << cursor->size << endl << endl;
     // shift everything left 1
     for(int j=i;j<cursor->size - 1;j++){
@@ -258,7 +257,7 @@ void BPTree::deleteInternal(Node* parent, int cursorI, int x){
     return;
   }
 
-  if(rightSibling != NULL && rightSibling->size > ceil(MAX/2)){
+  if(rightSibling != NULL && rightSibling->size > MAX/2.0){
     cout << "\nRedistributing from right sibling\n\n";
     // redistribute from rightSibling
     cursor->key[cursor->size-1] = rightSibling->key[0];
@@ -273,7 +272,7 @@ void BPTree::deleteInternal(Node* parent, int cursorI, int x){
     return;
   }
 
-  if(leftSibling != NULL && leftSibling->size > ceil(MAX/2)){
+  if(leftSibling != NULL && leftSibling->size > MAX/2.0){
     cout << "\nRedistributing from left sibling\n\n";
     cursor->ptr[cursor->size+1] = cursor->ptr[cursor->size];
     for(int j=cursor->size;j>0;j--){
@@ -340,6 +339,9 @@ void BPTree::deleteInternal(Node* parent, int cursorI, int x){
     deleteInternal(grandparent, parentI, parent->key[cursorI]);
     return;
   }
+      // else move a value from the right side to the end of cursor, update the cursor size and take the ptr with it, and then left shift the rightsibling and update its size
+      // finally, update the parents key[cursorI+1] to the new value of rightsibling->key[0]
+      // return
 }
 
 
