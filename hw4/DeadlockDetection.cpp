5d914e642a4bd954807ac730dc0596f1662ec08c
Trevor Grabham <tgrabham@sfu.ca>
2021-08-05 14:18:48 -0700

    added a few comments

--- hw4/DeadlockDetection.cpp
+++ hw4/DeadlockDetection.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 enum LockType {shared, exclusive};
 
 class Lock {
-    string resource;
     LockType type;
+    Transaction* owner;
 };
 
 class Transaction {
     unsigned id;
-    Lock* resourceLocks;
     unsigned numberOfLocks;
+    unsigned timeStarted;
 };
 
 class Resource {
-    LockType* locks
+    LockType* locks;
+
 }
 
-
+// read in the text file as one long string
+// split on white space
+// for each Transaction
+//     split on the colon
+//     for the first part, remove the T and read in the transaction number
+//     search our 
