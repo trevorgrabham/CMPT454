d73325ae123933379be1dc976749ecf74a665e54
tgrabham <tgrabham@sfu.ca>
2021-08-05 17:43:21 -0700

    maybe got a working graph?

--- hw4/DeadlockDetection.cpp
+++ hw4/DeadlockDetection.cpp
@@ -3,30 +3,69 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include <queue>
+#include <map>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
-enum LockType {shared, exclusive};
+enum LockType {shared, exclusive, none};
+enum ActionChar {R, W, X, S, C, A};
 
-class Lock {
-    LockType type;
-    Transaction* owner;
+class LockRequest {
+    Transaction* requestor;
+    LockType lockType;
 };
 
 class Transaction {
     unsigned id;
-    unsigned numberOfLocks;
-    unsigned timeStarted;
+    unsigned numberOfLocks;         // doubles as the length of lockedResources
+    Resource** lockedResources;     // null if we do not hold any locks currently
+    long double timeStarted;        
 };
 
 class Resource {
-    LockType* locks;
+    string resourceName;
+    unsigned numLocks;       // 0 if not locked, also used as length of owner
+    Transaction** owner;     // null if not locked
+    LockType lockType;      
+    queue<LockRequest*> requestQ;
+};
 
-}
+class Action {
+    Transaction* transaction;
+    Resource* resource;
+    ActionChar actionChar;
+};
+
+
+// Globals
+map<string, Resource*> resources;
+map<unsigned, Transaction*> transactions; 
+queue<Action*> actions;
+// End globals
+
 
 // read in the text file as one long string
 // split on white space
 // for each Transaction
 //     split on the colon
 //     for the first part, remove the T and read in the transaction number
-//     search our 
+//     create a transaction with the transaction number, starting at 0 seconds until we start the processing
+//     add the transaction to the global list
+//     for the second part, read the first letter, and store it as the action
+//     remove the brackets from the second part
+//     store the remaining second part as the resource name
+//     create and add the resource to the global list
+// we should now have a list of transactions, in order, so we store them in a queue
+// for each item in the queue
+//     if the transaction time = 0 (lookup using map.find)
+//         start the timer
+//     if action != X or S
+//         continue
+//     if action = S
+//         lookup action->resource->resourceName in resource table
+//         if the resource doesn't have an exclusive lock
+//             add action->transaction to the action->resource->
+//         else
---
+++ hw4/Graph.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+class Graph {
+    unsigned size;
+    vector<vector<unsigned>> adjacencyList;
+    void reachableFrom(unsigned source, unsigned numSteps, set<unsigned>* reachable);
+public:
+    Graph(unsigned n);
+    void addEdge(unsigned s, unsigned d);
+    vector<unsigned>* hasCycle();
+};
+
+Graph::Graph(unsigned n){
+    this->adjacencyList = vector<vector<unsigned>> (n, vector<unsigned>());
+    this->size = n;
+}
+
+void Graph::addEdge(unsigned s, unsigned d) {
+    this->adjacencyList[s-1].push_back(d-1);
+}
+
+void Graph::reachableFrom(unsigned source, unsigned numSteps, set<unsigned>* reachable){
+    if( numSteps >= this->size) {
+        return;
+    }
+    for(auto neighbour: this->adjacencyList[source]){
+        reachable->insert(neighbour);
+        this->reachableFrom(neighbour, numSteps+1, reachable);
+    }
+}
+
+vector<unsigned>* Graph::hasCycle(){
+    vector<unsigned>* res;
+    *res = vector<unsigned>();
+    vector<set<unsigned>> reachable = vector<set<unsigned>>(this->size, set<unsigned>());
+    for(unsigned i = 0;i<this->size;i++){       // for each vertex
+        this->reachableFrom(i+1, 0, &reachable[i]);
+    }
+    for(unsigned i = 0;i<this->size;i++){       // for each vertex
+        if(reachable[i].count(i) == 0){
+            res->push_back(i+1);
+        }
+    }
+    if(res->size() <= 0){
+        return NULL;
+    } else {
+        return res;
+    }
+}
+
+int main(){
+    Graph g(6);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.addEdge(3,1);
+    g.addEdge(3,2);
+    g.addEdge(4,3);
+    g.addEdge(5,6);
+    g.addEdge(6,5);
+    vector<unsigned>* cyclicVertices = g.hasCycle();
+    cout << "Cyclic vertices include: ";
+    for(auto v: *cyclicVertices){
+        cout << v << " ";
+    }
+    cout << endl;
+}
